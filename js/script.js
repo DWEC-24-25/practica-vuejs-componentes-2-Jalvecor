3057b09cdbc2cd5c7145298fc8601361d5daeb2d
Daniel Alcaraz <danalcbai@alu.edu.gva.es>
2025-02-18 18:58:40 +0100

    Puesto los prompt correctamente

--- js/script.js
+++ js/script.js
@@ -76,11 +76,12 @@ const ItemData = defineComponent({
     },
     template: `
         <div v-if="!editando">
-            <p><strong>Name </strong> </p>
+            <p><strong>{{ item.data.find(d => d.prompt === 'Name').prompt }}</strong> </p>
+
             <h3> {{ item.data.find(d => d.name === 'name').value }}</h3>
-            <p> <strong>Description </strong> <br>{{ item.data.find(d => d.name === 'description').value }}</p>
-            <p><strong>Director </strong> <br> {{ item.data.find(d => d.name === 'director').value }}</p>
-            <p><strong>Release Date:</strong> <br>{{ item.data.find(d => d.name === 'datePublished').value }}</p>
+            <p> <strong>{{ item.data.find(d => d.name === 'description').prompt}}</strong> <br>{{ item.data.find(d => d.name === 'description').value }}</p>
+            <p><strong>{{ item.data.find(d => d.name === 'director').prompt }} </strong> <br> {{ item.data.find(d => d.name === 'director').value }}</p>
+            <p><strong>{{ item.data.find(d => d.name === 'datePublished').prompt }}</strong> <br>{{ item.data.find(d => d.name === 'datePublished').value }}</p>
             <a :href="item.href" target="_blank" class="btn btn-primary">Ver</a>
                 <button @click="toggleEditFormVisibility" class="btn btn-secondary ms-2">Editar</button>
         </div>
